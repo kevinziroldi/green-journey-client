c0b12d54e3cfad213510cd707edd4ae5b495d2a3
kevinziroldi <k.ziroldi@gmail.com>
2024-09-25 16:48:15 +0200

    Initial commit

---
+++ GreenJourney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreenJourney
+//
+//  Created by Kevin Ziroldi on 25/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreenJourney/GreenJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenJourneyApp.swift
+//  GreenJourney
+//
+//  Created by Kevin Ziroldi on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
