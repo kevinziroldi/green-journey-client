ab207c2444443c5849be765242e18ed604cf5787
kevinziroldi <k.ziroldi@gmail.com>
2024-09-25 21:36:52 +0200

    created and tested ML model

--- GreenJourney/ContentView.swift
+++ GreenJourney/ContentView.swift
@@ -5,17 +5,45 @@
 //  Created by Kevin Ziroldi on 25/09/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var continent: String = "Asia"
+    @State private var temperature: String = "high_temp"
+    @State private var population: String = "high_pop"
+    @State private var city: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Continent: ")
+                TextField("Enter Continent", text: $continent)
+                
+                Text("Temperature: ")
+                TextField("Enter temperature", text: $temperature).textInputAutocapitalization(.never)
+                
+                Text("Population: ")
+                TextField("Enter population", text: $population).textInputAutocapitalization(.never)
+                
+                Text("City: " + city)
+            }
+            .navigationTitle("GreenJourney")
+            .toolbar {
+                Button("Calculate", action: computeCity)
+            }
         }
-        .padding()
+    }
+    
+    func computeCity() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try GreenJourneyClassifier(configuration: config)
+            let prediction = try model.prediction(average_temperature: temperature, population: population, continent: continent)
+            city = prediction.city
+        }catch{
+            city = "error"
+        }
     }
 }
 
