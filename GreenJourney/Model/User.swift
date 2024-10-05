3d6ae73a53a19b730a287034026bad90693d3325
matteo volpari <matteo.volpari@gmail.com>
2024-10-05 16:47:44 +0200

    small changes

--- GreenJourney/Model/User.swift
+++ GreenJourney/Model/User.swift
@@ -4,4 +4,36 @@
 //
 //  Created by matteo volpari on 05/10/24.
 //
+import Foundation
 
+class User {
+    var id: Int
+    var firstName: String
+    var lastname: String
+    var birthDate: Date
+    var gender: String
+    var firebaseUID: Int
+    var zipCode: Int
+    var streetName: String
+    var houseNumber: Int
+    var city: String
+    
+    var travels: [Travel]
+    var draftTravels: [Travel]
+    
+    init(id: Int, firstName: String, lastname: String, birthDate: Date, gender: String, firebaseUID: Int, zipCode: Int, streetName: String, houseNumber: Int, city: String) {
+        self.id = id
+        self.firstName = firstName
+        self.lastname = lastname
+        self.birthDate = birthDate
+        self.gender = gender
+        self.firebaseUID = firebaseUID
+        self.zipCode = zipCode
+        self.streetName = streetName
+        self.houseNumber = houseNumber
+        self.city = city
+        self.travels = []
+        self.draftTravels = []
+    }
+    
+}
---
+++ Travel.swift
@@ -0,0 +1,7 @@
+//
+//  Travel.swift
+//  GreenJourney
+//
+//  Created by matteo volpari on 05/10/24.
+//
+
