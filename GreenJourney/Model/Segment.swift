16741df4de024fe771a149d88e6a08e91f6fe204
matteo volpari <matteo.volpari@gmail.com>
2024-10-13 15:13:47 +0200

    Added enum for vehicle

--- GreenJourney/Model/Segment.swift
+++ GreenJourney/Model/Segment.swift
@@ -12,10 +12,24 @@ class Segment: Decodable {
     var destination: String
     var dateTime: Date
     var duration: Duration
-    var vehicle: String
+    var vehicle: Vehicle
     var description: String
     var price: Float64
     var co2Emitted: Float64
     var numSegment: Int
     var travelID: Int
+    
+    init(segmentID: Int, departure: String, destination: String, dateTime: Date, duration: Duration, vehicle: Vehicle, description: String, price: Float64, co2Emitted: Float64, numSegment: Int, travelID: Int) {
+        self.segmentID = segmentID
+        self.departure = departure
+        self.destination = destination
+        self.dateTime = dateTime
+        self.duration = duration
+        self.vehicle = vehicle
+        self.description = description
+        self.price = price
+        self.co2Emitted = co2Emitted
+        self.numSegment = numSegment
+        self.travelID = travelID
+    }
 }
---
+++ GreenJourney/Model/Vehicle.swift
@@ -0,0 +1,14 @@
+//
+//  Vehicle.swift
+//  GreenJourney
+//
+//  Created by matteo volpari on 13/10/24.
+//
+
+enum Vehicle: Decodable {
+    case car
+    case bus
+    case train
+    case plane
+    case bike
+}
