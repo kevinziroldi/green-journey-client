00c4b268b3d1fd1b2e024735f608dce64a038d12
kevinziroldi <k.ziroldi@gmail.com>
2024-10-31 10:20:00 +0100

    added config file for server ip

--- GreenJourney/Network/NetworkManager.swift
+++ GreenJourney/Network/NetworkManager.swift
@@ -1,11 +1,27 @@
 import Foundation
 import Combine
 
+struct Config {
+    static var serverIP: String? {
+        guard let url = Bundle.main.url(forResource: "Config", withExtension: "plist"),
+              let data = try? Data(contentsOf: url),
+              let config = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
+        else { return nil }
+        return config["ServerIP"] as? String
+    }
+}
+
 class NetworkManager {
     static let shared = NetworkManager()
-    private let baseURL = "http://192.168.5.11:8080"
+    private var baseURL = ""
     
-    private init() {}
+    private init() {
+        if let serverIP = Config.serverIP {
+            baseURL = "http://" + serverIP + ":8888"
+        }else {
+            print("Error loading server IP from Config")
+        }
+    }
     
     func getBaseURL() -> String {
         return baseURL
