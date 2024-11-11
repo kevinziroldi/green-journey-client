d9d2856d3a3131c598b8a7aea8c74147dd948d2f
kevinziroldi <k.ziroldi@gmail.com>
2024-11-11 17:13:37 +0100

    fixed navoigation bar

--- GreenJourney/View/MainView.swift
+++ GreenJourney/View/MainView.swift
@@ -6,33 +6,41 @@ struct MainView: View {
     @Environment(\.modelContext) private var modelContext
     @State private var selectedTab = 1
     
+    @State var navigationPath: NavigationPath = NavigationPath()
+    
     init(modelContext: ModelContext) {
         _viewModel = StateObject(wrappedValue: MainViewModel(modelContext: modelContext))
+        navigationPath = NavigationPath()
     }
     
     var body: some View {
         if viewModel.checkUserLogged() {
-            TabView(selection: $selectedTab) {
-                RankingView()
-                    .tabItem {
-                        Label("Ranking", systemImage: "star")
-                    }
-                    .tag(0)
-                
-                FromToView(modelContext: modelContext)
-                    .tabItem {
-                        Label("From-To", systemImage: "location")
-                    }
-                    .tag(1)
-                
-                TravelsView(modelContext: modelContext)
-                    .tabItem {
-                        Label("My travels", systemImage: "airplane")
-                    }
-                    .tag(2)
-            }.onAppear {
-                // refresh travels data
-                viewModel.fetchTravels()
+            NavigationStack (path: $navigationPath) {
+                TabView(selection: $selectedTab) {
+                    RankingView()
+                        .tabItem {
+                            Label("Ranking", systemImage: "star")
+                        }
+                        .tag(0)
+                    
+                    FromToView(modelContext: modelContext)
+                        .tabItem {
+                            Label("From-To", systemImage: "location")
+                        }
+                        .tag(1)
+                    
+                    TravelsView(modelContext: modelContext)
+                        .tabItem {
+                            Label("My travels", systemImage: "airplane")
+                        }
+                        .tag(2)
+                }.onAppear {
+                    // refresh travels data
+                    viewModel.fetchTravels()
+                }
+            }
+            .onAppear {
+                navigationPath = NavigationPath()
             }
         }else {
             LoginView(modelContext: modelContext)
