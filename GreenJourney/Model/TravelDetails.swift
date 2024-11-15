efa394a135b45ba73bdfd72e860dd6c210ef8346
kevinziroldi <k.ziroldi@gmail.com>
2024-11-15 22:22:09 +0100

    moved first and last segment in traveldetails

--- GreenJourney/Model/TravelDetails.swift
+++ GreenJourney/Model/TravelDetails.swift
@@ -14,4 +14,30 @@ class TravelDetails: Codable, Identifiable {
         case travel = "travel"
         case segments = "segments"
     }
+    
+    func getFirstSegment() -> Segment? {
+        if let firstListSegment = self.segments.first {
+            var firstSegment = firstListSegment
+            for segment in self.segments {
+                if segment.numSegment < firstSegment.numSegment {
+                    firstSegment = segment
+                }
+            }
+            return firstSegment
+        }
+        return nil
+    }
+    
+    func getLastSegment() -> Segment? {
+        if let firstListSegment = self.segments.first {
+            var lastSegment = firstListSegment
+            for segment in self.segments {
+                if segment.numSegment > lastSegment.numSegment {
+                    lastSegment = segment
+                }
+            }
+            return lastSegment
+        }
+        return nil
+    }
 }
