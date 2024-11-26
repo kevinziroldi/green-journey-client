6ca10b921b907b7bc412924908e2b04b42372f3b
Matteo Volpari <matteo.volpari@gmail.com>
2024-11-26 10:30:49 +0100

    fixed error in LoginView and SignUpView

--- GreenJourney/Features/Authentication/Implementation/LoginView.swift
+++ GreenJourney/Features/Authentication/Implementation/LoginView.swift
@@ -19,116 +19,116 @@ struct LoginView: View {
                 .resizable()
                 .padding()
                 .aspectRatio(contentMode: .fit)
-        }
-        ScrollView {
-            VStack {
-                Spacer()
-                
-                TextField("Email", text: $viewModel.email)
-                    .textInputAutocapitalization(.never)
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                    .padding()
-                    .frame(maxWidth: 300)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                    .frame(maxWidth: 300)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                HStack {
-                    if let resendMessage = viewModel.resendEmail {
-                        Text(resendMessage)
-                    }
-                    Button("Reset password") {
-                        viewModel.resetPassword()
-                    }
-                }
-                // error message
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .font(.caption)
-                }
-                
-                VStack(spacing: 20) {
-                    Button(action: {
-                        viewModel.login()
-                    }) {
-                        Text("Login")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.blue)
-                            .cornerRadius(30)
-                    }
-                    .fullScreenCover(isPresented: $viewModel.isEmailVerificationActive) {
-                        EmailVerificationView(viewModel: viewModel)
-                    }
-                    
-                    HStack {
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                        
-                        Text("OR")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                        
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.horizontal, 16)
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(Color.gray, lineWidth: 1)
-                            .fill(Color.white)
-                            .shadow(radius: 1)
-                            
-                            
-                        Button(action: {
-                            Task{await viewModel.signInWithGoogle()}
-                                }) {
-                                    HStack(spacing: 10) {
-                                        Image("googleLogo")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 60)
-                                            .safeAreaPadding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                                        Spacer()
-                                        Text("Sign in with Google")
-                                            .foregroundStyle(.black)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                    }
-                                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
-                                    
-                                }
-                        }
-                    
-                    
+            ScrollView {
+                VStack {
                     Spacer()
                     
+                    TextField("Email", text: $viewModel.email)
+                        .textInputAutocapitalization(.never)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                        .padding()
+                        .frame(maxWidth: 300)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    
+                    SecureField("Password", text: $viewModel.password)
+                        .padding()
+                        .frame(maxWidth: 300)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    HStack {
+                        if let resendMessage = viewModel.resendEmail {
+                            Text(resendMessage)
+                        }
+                        Button("Reset password") {
+                            viewModel.resetPassword()
+                        }
+                    }
+                    // error message
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .font(.caption)
+                    }
+                    
+                    VStack(spacing: 20) {
+                        Button(action: {
+                            viewModel.login()
+                        }) {
+                            Text("Login")
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(30)
+                        }
+                        .fullScreenCover(isPresented: $viewModel.isEmailVerificationActive) {
+                            EmailVerificationView(viewModel: viewModel)
+                        }
+                        
+                        HStack {
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                            
+                            Text("OR")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal, 16)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(Color.gray, lineWidth: 1)
+                                .fill(Color.white)
+                                .shadow(radius: 1)
+                            
+                            
+                            Button(action: {
+                                Task{await viewModel.signInWithGoogle()}
+                            }) {
+                                HStack(spacing: 10) {
+                                    Image("googleLogo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60)
+                                        .safeAreaPadding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                                    Spacer()
+                                    Text("Sign in with Google")
+                                        .foregroundStyle(.black)
+                                    Spacer()
+                                    Spacer()
+                                    Spacer()
+                                }
+                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                                
+                            }
+                        }
+                        
+                        
+                        Spacer()
+                        
+                    }
+                }
+                .padding()
+                .fullScreenCover(isPresented: $viewModel.isLogged) {
+                    MainView(modelContext: modelContext)
                 }
             }
-            .padding()
-            .fullScreenCover(isPresented: $viewModel.isLogged) {
-                MainView(modelContext: modelContext)
+            .scrollDismissesKeyboard(.interactively)
+            
+            Button ("Sign up") {
+                isNavigationActive = true
             }
-            .navigationBarHidden(true)
+            .fullScreenCover(isPresented: $isNavigationActive) {
+                SignUpView(viewModel: viewModel)
+            }
         }
-        .scrollDismissesKeyboard(.interactively)
-        
-        Button ("Sign up") {
-            isNavigationActive = true
-        }
-        .fullScreenCover(isPresented: $isNavigationActive) {
-            SignUpView(viewModel: viewModel)
-        }
+        .navigationBarHidden(true)
     }
 }
--- GreenJourney/Features/Authentication/Implementation/SignUpView.swift
+++ GreenJourney/Features/Authentication/Implementation/SignUpView.swift
@@ -6,103 +6,105 @@ struct SignUpView: View {
     @Environment(\.modelContext) private var modelContext
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Sign Up")
-                    .font(.largeTitle)
-                    .padding(.bottom, 32)
-                
-                Image("loginLogo")
-                    .resizable()
-                    .padding()
-                    .aspectRatio(contentMode: .fit)
-                
-                TextField("Email", text: $viewModel.email)
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                SecureField("Repeat Password", text: $viewModel.repeatPassword)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                
-                TextField("First name", text: $viewModel.firstName)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                
-                TextField("Last name", text: $viewModel.lastName)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                // error message
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .font(.caption)
-                }
-                
-                VStack (spacing: 20) {
-                    Button(action: {
-                        viewModel.signUp()
-                    }) {
-                        Text("Create account")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.blue)
-                            .cornerRadius(8)
-                    }
-                    .fullScreenCover(isPresented: $viewModel.isEmailVerificationActive) {
-                        EmailVerificationView(viewModel: viewModel)
-                    }
+        VStack {
+            ScrollView {
+                VStack {
+                    Text("Sign Up")
+                        .font(.largeTitle)
+                        .padding(.bottom, 32)
                     
-                    HStack {
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                        
-                        Text("OR")
+                    Image("loginLogo")
+                        .resizable()
+                        .padding()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    TextField("Email", text: $viewModel.email)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    
+                    SecureField("Password", text: $viewModel.password)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    SecureField("Repeat Password", text: $viewModel.repeatPassword)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    
+                    TextField("First name", text: $viewModel.firstName)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    
+                    TextField("Last name", text: $viewModel.lastName)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                    // error message
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
                             .font(.caption)
-                            .foregroundColor(.gray)
+                    }
+                    
+                    VStack (spacing: 20) {
+                        Button(action: {
+                            viewModel.signUp()
+                        }) {
+                            Text("Create account")
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(8)
+                        }
+                        .fullScreenCover(isPresented: $viewModel.isEmailVerificationActive) {
+                            EmailVerificationView(viewModel: viewModel)
+                        }
                         
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.horizontal, 16)
-                    
-                    Button (action: {Task{await viewModel.signInWithGoogle()}}){
-                        Image("googleLogo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 40)
-                            .padding(.trailing, 8)
-                        Text("Sign in with Google")
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.bordered)
-                    
-                    Spacer()
-                    Button ("Login") {
-                        isNavigationLoginActive = true
-                    }
-                    .fullScreenCover(isPresented: $isNavigationLoginActive) {
-                        LoginView(modelContext: modelContext)
+                        HStack {
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                            
+                            Text("OR")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal, 16)
+                        
+                        Button (action: {Task{await viewModel.signInWithGoogle()}}){
+                            Image("googleLogo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 40)
+                                .padding(.trailing, 8)
+                            Text("Sign in with Google")
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.bordered)
+                        
+                        Spacer()
+                        Button ("Login") {
+                            isNavigationLoginActive = true
+                        }
+                        .fullScreenCover(isPresented: $isNavigationLoginActive) {
+                            LoginView(modelContext: modelContext)
+                        }
                     }
                 }
+                .padding()
             }
-            .padding()
-            .navigationBarHidden(true)
+            .scrollDismissesKeyboard(.interactively)
         }
-        .scrollDismissesKeyboard(.interactively)
+        .navigationBarHidden(true)
     }
 }
