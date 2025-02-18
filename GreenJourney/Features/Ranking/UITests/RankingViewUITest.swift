32e412145990073517c33f6481352c1e940db8f4
kevinziroldi <k.ziroldi@gmail.com>
2025-02-18 09:34:13 +0100

    added ui tests files

---
+++ GreenJourney/Features/Ranking/UITests/RankingViewUITest.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+final class RankingViewUITest: XCTestCase {
+    let app = XCUIApplication()
+    let timer = 5.0
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launchArguments.append("ui_tests")
+        app.launch()
+    }
+    
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+}
