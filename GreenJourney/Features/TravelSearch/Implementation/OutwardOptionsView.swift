28aad8dc14b30906e838a029ffc835ac0854b12e
Matteo Volpari <matteo.volpari@gmail.com>
2025-03-06 16:56:15 +0100

    removed replicated code

--- GreenJourney/Features/TravelSearch/Implementation/OutwardOptionsView.swift
+++ GreenJourney/Features/TravelSearch/Implementation/OutwardOptionsView.swift
@@ -5,57 +5,8 @@ struct OutwardOptionsView: View {
     @Binding var navigationPath: NavigationPath
     
     var body: some View {
-        VStack (spacing: 0){
-            HStack {
-                // departure label
-                Text(viewModel.departure.cityName)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding()
-                    .accessibilityIdentifier("departureLabel")
-                
-                // line departure - destination
-                GeometryReader { geometry in
-                    ZStack {
-                        Path { path in
-                            path.move(to: CGPoint(x: 0, y: geometry.size.height/2))
-                            
-                            path.addQuadCurve(
-                                to: CGPoint(x: geometry.size.width, y: geometry.size.height/2),
-                                control: CGPoint(x: geometry.size.width/2, y: 0)
-                            )
-                        }
-                        .stroke(style: StrokeStyle(lineWidth: 4, dash: [15, 8]))
-                        .foregroundColor(.primary)
-                        
-                        Circle()
-                            .stroke(Color.black, lineWidth: 5)
-                            .fill(Color.white)
-                            .frame(width: 10, height: 10)
-                            .position(x: geometry.size.width, y: geometry.size.height/2)
-                        
-                        Circle()
-                            .stroke(Color.black, lineWidth: 5)
-                            .fill(Color.white)
-                            .frame(width: 10, height: 10)
-                            .position(x: 0, y: geometry.size.height/2)
-                    }
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                }
-                .frame(width: 180, height: 75, alignment: .top)
-                .accessibilityIdentifier("fromToLine")
-                
-                // destination label
-                Text(viewModel.arrival.cityName)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding()
-                    .accessibilityIdentifier("destinationLabel")
-            }
-            
-            Text(viewModel.datePicked.formatted(date: .numeric, time: .shortened))
-                .accessibilityIdentifier("datePicked")
-        }
+        HeaderView(from: viewModel.departure.cityName, to: viewModel.arrival.cityName, date: viewModel.datePicked)
+            .overlay(Color.clear.accessibilityIdentifier("headerView"))
         
         Rectangle()
             .frame(height: 1)
