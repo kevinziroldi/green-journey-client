501ac8a900e0c0dffb1f45e945ecd9d6a2c220ca
Matteo Volpari <matteo.volpari@gmail.com>
2025-04-04 22:34:51 +0200

    removed comment in device type selection

--- GreenJourney/Utilities/UITestsDeviceSize.swift
+++ GreenJourney/Utilities/UITestsDeviceSize.swift
@@ -8,15 +8,6 @@ enum DeviceSize: String {
 
 class UITestsDeviceSize {
     static var deviceSize: DeviceSize {
-        /*let bundle = Bundle(for: UITestsDeviceSize.self)
-        guard let url = bundle.url(forResource: "Config", withExtension: "plist"),
-              let data = try? Data(contentsOf: url),
-              let config = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
-        else { return .compact }
-        if let stringValue = config["UITestsDeviceSize"] as? String {
-            return DeviceSize(rawValue: stringValue) ?? .compact
-        }
-        return .compact*/
         if UIDevice.current.userInterfaceIdiom == .pad{
             return .regular
         }
